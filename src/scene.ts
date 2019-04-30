64d1e3d4439112bd5d6b0af3544f5e8e05029c5b
Rupert McKay <rupert.mckay@softwire.com>
2019-04-30 16:08:10 +0100

    Minor: moved a point in the scene to make it look nicer

--- src/scene.ts
+++ src/scene.ts
@@ -7,7 +7,7 @@ export class Scene {
     constructor() {
         this.polys = [];
         // TODO just a test scene for now
-        this.polys.push(new Poly([new Point(100, 100, 10), new Point(200, 300, 10), new Point(300, 100, 10)], new Colour(255, 0, 0, 1)));
+        this.polys.push(new Poly([new Point(100, 100, 10), new Point(200, 400, 10), new Point(300, 100, 10)], new Colour(255, 0, 0, 1)));
         this.polys.push(new Poly([new Point(100, 300, 20), new Point(200, 100, 20), new Point(300, 300, 20)], new Colour(0, 255, 0, 1)));
     }
 
